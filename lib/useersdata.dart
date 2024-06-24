878cc6e67d46076c55919e843ac8e95df99afb4e
Rehab <rehabsobhy184@gmail.com>
2024-06-25 02:01:54 +0300

    add imgpath and imgname

--- lib/useersdata.dart
+++ lib/useersdata.dart
@@ -1,14 +1,21 @@
+import 'dart:typed_data';
+
 class UsersData {
   String? name;
   String? password;
   String? email;
   String? title;
+  String? imgname;
+  Uint8List imgpath;
 
-  UsersData(
-      {required this.password,
-      required this.email,
-      required this.name,
-      required this.title});
+  UsersData({
+    required this.imgname,
+    required this.email,
+    required this.password,
+    required this.imgpath,
+    required this.name,
+    required this.title,
+  });
 
   Map<String, dynamic> ToMap() {
     return {
