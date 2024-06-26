590e95b480e157e56a4c25b23b735d4f0591ea41
Rehab <rehabsobhy184@gmail.com>
2024-06-26 08:50:04 +0300

    add main page

--- lib/features/auth/mainpage.dart
+++ lib/features/auth/mainpage.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_instagram_clone/auth/auth_screen.dart';
-import 'package:flutter_instagram_clone/screen/home.dart';
-import 'package:flutter_instagram_clone/widgets/navigation.dart';
+
+import 'package:instagram/features/auth/auth_screen.dart';
+
+import '../../utils/widgets/navigation.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
