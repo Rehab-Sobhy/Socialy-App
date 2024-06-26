abb34840b6c4c57c81c542c1ba37d1d4f367f2df
Rehab <rehabsobhy184@gmail.com>
2024-06-26 08:50:23 +0300

    eddit on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,26 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:instagram/features/auth/Register.dart';
-
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:instagram/features/auth/mainpage.dart';
 import 'package:instagram/firebase_options.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const RegisterView(),
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: MainPage(),
     );
   }
 }
